d925dbfab734b72561150e35229ed3b4e428d74f
MDTAHSINURRAHMAN <mdtahsinurrahman1210@gmail.com>
2025-05-05 03:42:56 +0600

    ..

--- server.js
+++ server.js
@@ -22,10 +22,17 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        "http://localhost:3000", // dev
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
