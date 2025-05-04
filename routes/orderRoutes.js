8be1e028f34b5bc7fcea83016bbd0dad3539b713
MDTAHSINURRAHMAN <mdtahsinurrahman1210@gmail.com>
2025-05-04 22:54:02 +0600

    blah

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -7,11 +7,10 @@ import {
   deleteOrder,
   updateOrder,
 } from "../controllers/orderController.js";
-import { validateOrder } from "../middlewares/validateOrder.js";
 import { protect } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
-router.post("/", validateOrder, createOrder);
+router.post("/", createOrder);
 router.get("/", protect, getAllOrders);
 router.get("/:id", protect, getOrderById);
 router.put("/:id/status", protect, updateOrderStatus);
