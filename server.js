6ff26cba5530d9a4305bddfdf6b1366a0545db0a
MDTAHSINURRAHMAN <mdtahsinurrahman1210@gmail.com>
2025-05-05 23:40:05 +0600

    >>

--- server.js
+++ server.js
@@ -29,7 +29,9 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // ✅ your frontend URL (change in production)
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    //  // ✅ your frontend URL (change in production)
+
     credentials: true,               // ✅ must be true to allow cookie sending
   })
 );
